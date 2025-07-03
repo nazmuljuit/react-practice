722de311701406694d9179de72550020a6aed1ec
Nazmul Hossain <nazmuljuit@gmail.com>
2025-07-03 14:42:46 +0600

    :pencil2: Addd:=> Implement first dynamic values

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,10 @@
+const name = ['Nazmul','Hossain'];
+
 function Post(){
+    const selectName = Math.random() > 0.5 ? name[0]:name[1];
     return(
         <div>
-            <p>My Name is Nazmul Hossain</p>
+            <p>My Name is {selectName}</p>
             <p>I am 38 years old</p>
         </div>
     );
