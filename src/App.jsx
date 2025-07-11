2b8f70fd76f9ec600e0dc774d476b2319434a3d0
Nazmul Hossain <nazmuljuit@gmail.com>
2025-07-11 10:42:30 +0600

    :pencil2: Add:=> Implement props

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import Post from './components/Post'
 
+const name = ['Nazmul','Hossain'];
 function App() {
   const [count, setCount] = useState(0)
-
+  const selectName = Math.random() > 0.5 ? name[0]:name[1];
   return (
-    <Post/>
+    <Post name = {selectName}/>
   )
 }
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,10 @@
-const name = ['Nazmul','Hossain'];
 
-function Post(){
-    const selectName = Math.random() > 0.5 ? name[0]:name[1];
+
+function Post(props){
+    
     return(
         <div>
-            <p>My Name is {selectName}</p>
+            <p>My Name is {props.name}</p>
             <p>I am 38 years old</p>
         </div>
     );
