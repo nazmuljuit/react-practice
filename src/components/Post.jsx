a379732ae8cffdc0ce1021a947d1f817331cc37b
Nazmul Hossain <nazmuljuit@gmail.com>
2025-07-11 11:14:59 +0600

    :pencil2: Add:=> Implement external css

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,9 @@
-
+import classes from './Post.module.css'
 
 function Post(props){
     
     return(
-        <div>
+        <div className={classes.post}>
             <p>My Name is {props.name}</p>
             <p>I am 38 years old</p>
         </div>
